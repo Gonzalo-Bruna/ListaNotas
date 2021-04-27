24a07a516a1f607b241bc70752e50f8ea0a00f71
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-04-26 20:23:31 -0400

    forgot to save changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CrearNotaComponent } from '../app/aplicacion/crear-nota/crear-nota.component';
 import { ListaNotasComponent } from '../app/aplicacion/lista-notas/lista-notas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "crearNota", component: CrearNotaComponent},
+  {path: "listaNotas", component: ListaNotasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
