2c4354de229924003ab3bd51e5d16d5549f614c1
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-04-26 20:21:42 -0400

    added routes for the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearNotaComponent } from '../app/aplicacion/crear-nota/crear-nota.component';
+import { ListaNotasComponent } from '../app/aplicacion/lista-notas/lista-notas.component';
 
 const routes: Routes = [];
 
